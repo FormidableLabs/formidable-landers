80b19c5267c18a79775336d89b721b081b50eafa
David Davidson <daviddavidson.1988@gmail.com>
2015-11-25 11:56:08 -0800

    * Set up core export infra * Port in header, footer, sidebar, themes

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import Header from "./components/header";
+import Footer from "./components/footer";
+import VictorySidebar from "./components/victory-sidebar";
+
+export default {
+  Header,
+  Footer,
+  VictorySidebar
+};
