2befcfc0e8c45a3b2c0b0ea9d58ce9a4689b4c97
Paula Lavalle <lavalle.paula@gmail.com>
2015-12-02 11:43:06 -0800

    Export VictorySettings VictorySidebar and VictoryTheme

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import Header from "./components/header";
 import Footer from "./components/footer";
+import VictorySettings from "./themes/victory-settings";
 import VictorySidebar from "./components/victory-sidebar";
+import VictoryTheme from "./themes/victory";
 
 export default {
   Header,
   Footer,
-  VictorySidebar
+  VictorySettings,
+  VictorySidebar,
+  VictoryTheme
 };
