84c491856b7a0b87a651ecbe654ce35af739bfa6
Paula Lavalle <lavalle.paula@gmail.com>
2015-12-02 16:23:09 -0800

    lint

--- src/themes/victory-settings.js
+++ src/themes/victory-settings.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 module.exports = {
   // Fonts
   sansSerif: "'Karla', 'Helvetica Neue', Helvetica, Arial, sans-serif",
@@ -23,3 +24,4 @@ module.exports = {
   paleRed: "#bd4139",
   red: "#bd1e13"
 };
+/* eslint-enable max-len */
