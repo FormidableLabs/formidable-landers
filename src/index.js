52162ee69f7a4c6e3332fffc866d6a86f46b5a68
Paula Lavalle <lavalle.paula@gmail.com>
2016-03-01 11:41:02 -0800

    Don't export logos
    I don’t have a use case for this.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-import Logos from "./assets/logos";
 import Header from "./components/header";
 import Footer from "./components/footer";
 import VictorySettings from "./themes/victory-settings";
 import VictoryTheme from "./themes/victory";
 
 export default {
-  Logos,
   Header,
   Footer,
   VictorySettings,
