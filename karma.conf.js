8a25445dd4398d74d1c193c33ba41f84df80fbe1
Paula Lavalle <lavalle.paula@gmail.com>
2016-03-02 19:43:36 -0800

    WIP Add test suite

---
+++ karma.conf.js
@@ -0,0 +1,50 @@
+var webpackConfig = require("./webpack.config.test");
+
+module.exports = function(config) {
+  config.set({
+    basePath: "",
+    frameworks: ["mocha"],
+    browsers: ["PhantomJS"],
+    reporters: ["spec"],
+    port: 9876,
+    colors: true,
+    autoWatch: true,
+    singleRun: false,
+
+    files: [
+      // all files in `tests/`
+      "test/**/*.js"
+      // each file acts as entry point for the webpack configuration
+    ],
+
+    preprocessors: {
+      // add webpack as preprocessor
+      "src/**/*.jsx": ["webpack", "sourcemap"],
+      "test/**/*.js": ["webpack", "sourcemap"]
+    },
+
+    // karma watches the test entry points
+    // (you don't need to specify the entry option)
+    // webpack watches dependencies
+    webpack: webpackConfig,
+
+    webpackServer: {
+      noInfo: true
+    },
+
+    client: {
+      mocha: {
+        ui: "bdd"
+      }
+    },
+
+    plugins: [
+      "karma-chrome-launcher",
+      "karma-mocha",
+      "karma-phantomjs-launcher",
+      "karma-sourcemap-loader",
+      "karma-spec-reporter",
+      "karma-webpack"
+    ]
+  })
+};
