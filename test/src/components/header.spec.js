4c4f1f74e0b43c2cbdb149fae20f70e9b3b59caf
Paula Lavalle <lavalle.paula@gmail.com>
2016-03-08 15:53:30 -0800

    Add tests for Header component

--- test/src/components/header.spec.js
+++ test/src/components/header.spec.js
@@ -1,23 +1,48 @@
 import React from "react";
-import { shallow, mount, render } from "enzyme";
+import { shallow } from "enzyme";
 
 import Header from "../../../src/components/header.jsx";
 
-describe("Header component", () => {
-  it("contains spec with an expectation", () => {
-    const wrapper = shallow(<Header />);
-    expect(wrapper.contains(<div className="foo" />)).toBe(true);
+describe("Header", () => {
+
+  describe("<header>", () => {
+    it("has default styles", () => {
+      const header = shallow(<Header />);
+      expect(header.props()).to.have.property("style");
+    });
+
+    it("accepts custom styles", () => {
+      const header = shallow(<Header styleOverrides={{textAlign: "left"}} />);
+      expect(header.props().style).to.have.property("textAlign", "left");
+    });
   });
 
-  it("contains spec with an expectation", () => {
-    expect(shallow(<Header />).is('.foo')).toBe(true);
-  });
+  describe("call to action", () => {
+    it("is a mailto:hello@formidable.com link", () => {
+      const headerLink = shallow(<Header />).find('a');
+      expect(headerLink).to.have.length(1);
+      expect(headerLink.props()).to.have.property("href", "mailto:hello@formidable.com");
+    });
 
-  it("contains spec with an expectation", () => {
-    expect(mount(<Header />).find('.foo').length).toBe(1);
-  });
+    it("has a default message", () => {
+      const headerLink = shallow(<Header />).find('a');
+      expect(headerLink.text()).to.equal("Need React.js consulting? Let’s talk.");
+    });
 
-  it("can run an expectation with render", () => {
-    expect(render(<Header />).find('.foo').length).toBe(1);
+    it("accepts a custom message", () => {
+      const headerLink = shallow(<Header>Need help leveling up? Contact us!</Header>).find('a');
+      expect(headerLink.text()).to.equal("Need help leveling up? Contact us!");
+      expect(headerLink.props()).to.have.property("href", "mailto:hello@formidable.com");
+    });
+
+    it("has default styles", () => {
+      const headerLink = shallow(<Header />).find('a');
+      expect(headerLink.props()).to.have.property("style");
+    });
+
+    it("accepts custom styles", () => {
+      const headerLink = shallow(<Header linkStyles={{lineHeight: 2}} />).find('a');
+      expect(headerLink.props().style).to.have.property("lineHeight", 2);
+    });
   });
 });
