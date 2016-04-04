eb18194c7d8fcd81e6349e5c85ac6036e7968caf
Paula Lavalle <lavalle.paula@gmail.com>
2016-04-04 12:38:22 -0700

    Update settings

--- src/themes/victory-settings.js
+++ src/themes/victory-settings.js
@@ -1,27 +1,40 @@
 /* eslint-disable max-len */
 export default {
+  // ---------------------
   // Fonts
+  // ---------------------
   sansSerif: "'Karla', 'Helvetica Neue', Helvetica, Arial, sans-serif",
-  serif: "'Didot', 'Bodoni MT', 'Cochin', 'Baskerville', 'Goudy Old Style', 'Bitstream Charter', serif",
+  serif: "'Georgia', serif",
   monospace: "'Source Code Pro', 'Inconsolata', 'Courier New', 'Courier', monospace",
+  serifHeadline: "'Didot', 'Bodoni MT', 'Cochin', 'Baskerville', 'Goudy Old Style', 'Bitstream Charter', serif",
+  // ---------------------
   // Colors
-  // Navy
-  deepNavy: "#1b2633",
-  navy: "#2b303b",
+  // ---------------------
   // Sand
-  whiteSand: "#ede7e1",
-  palestSand: "#ebe3db",
-  palerSand: "#e1d7cd",
+  whiteSand: "#f6f2ee",
+  palestSand: "#e8e0d8",
+  palerSand: "#e1d7cd", //bg
   paleSand: "#d1c7bc",
   sand: "#b5aca3",
-  darkSand: "#91887e",
-  darkerSand: "#67615c",
-  darkestSand: "#4d4945",
+  darkSand: "#948d85",
+  darkerSand: "#807973",
+  darkestSand: "#67615c",
+  // Mud
+  paleMud: "#4d4945",
   mud: "#34302e",
+  darkMud: "#181615",
   // Red
-  palestRed: "#E5847D",
-  palerRed: "#C75B54",
   paleRed: "#bd4139",
-  red: "#bd1e13"
+  red: "#bd1e13",
+  // ---------------------
+  // Layout
+  // ---------------------
+  gutter: 20,
+  // ---------------------
+  // Layout
+  // ---------------------
+  mediaQueries: {
+    medium: "only screen and (min-width: 800px)"
+  }
 };
 /* eslint-enable max-len */
