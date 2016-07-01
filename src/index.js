0e16e78dbbefad7c1fe940df3ed72bef9ced7c9f
Kyle Cesmat <kyle@kylecesmat.com>
2016-07-01 11:21:33 -0700

    add function for internal/external link

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import Header from "./components/header";
 import Footer from "./components/footer";
 import VictorySettings from "./themes/victory-settings";
 import VictoryTheme from "./themes/victory";
+import externalLinkIcons from "./markdown-renderers/external-link-icons";
 
 export {
   Header,
   Footer,
   VictorySettings,
-  VictoryTheme
+  VictoryTheme,
+  externalLinkIcons
 };
