1a980d052604e80bedd6d4bfc7a52b31ccfe516a
Kyle Cesmat <kyle.cesmat@formidable.com>
2016-07-04 09:34:23 -0700

    rename function, fix formatting

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import Header from "./components/header";
 import Footer from "./components/footer";
 import VictorySettings from "./themes/victory-settings";
 import VictoryTheme from "./themes/victory";
-import externalLinkIcons from "./markdown-renderers/external-link-icons";
+import appendLinkIcon from "./markdown-renderers/append-link-icon";
 
 export {
   Header,
   Footer,
   VictorySettings,
   VictoryTheme,
-  externalLinkIcons
+  appendLinkIcon
 };
--- src/markdown-renderers/append-link-icon.js
+++ src/markdown-renderers/append-link-icon.js
@@ -8,7 +8,9 @@ export default {
     const Icon = href.includes("formidable.com") ? IconInternalLink : IconExternalLink;
 
     return (
-      `<a href=${href} title=${title && title}>${text} <span style="margin: 0; padding: 0; display: inline-block; vertical-align: middle;">${Icon}</span></a>`
+      `<a href=${href} title=${title}>
+        ${text} <span style="margin: 0; padding: 0; display: inline-block; vertical-align: middle;">${Icon}</span>
+      </a>`
     );
   }
 };
